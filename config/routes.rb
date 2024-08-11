a56833dfaf13143a51209858ff7feb411fe2947f
asvirts <asvirts@outlook.com>
2024-08-11 17:55:23 -0500

    ruby new

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  
+  get "/articles/:id", to: "articles#show"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+end
